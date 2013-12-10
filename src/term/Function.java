a3003bd6fbfa8ef68518bb7f6b1c80f7cf230969
mina.workspace@gmail.com <mina.workspace@gmail.com@5e1f2a12-4c9f-fb57-4e15-150735339848>
2013-12-10 16:50:41 +0000

    Adding more documentation

--- src/term/Function.java
+++ src/term/Function.java
@@ -7,28 +7,39 @@ import java.util.List;
 import parser.FOLVisitor;
 
 
-
+// this class is used to represent a Function
 public class Function implements Term {
+	// a function name EX: f
 	private String functionName;
-	private List<Term> terms = new ArrayList<Term>();
+	// list of terms of the functions EX: (x,y)
+	private List<Term> terms = new ArrayList<Term>(); 
+	// a string representation of the function  EX: f(x,y)
 	private String stringRep = null;
+	// a hash code for having a unique value to identify the object
 	private int hashCode = 0;
 
+	
+	// a constructor tot create a function given a function name and its terms
 	public Function(String functionName, List<Term> terms) {
+		// set the function name
 		this.functionName = functionName;
+		// set the terms
 		this.terms.addAll(terms);
 	}
 
+	// return the function name
 	public String getFunctionName() {
 		return functionName;
 	}
 
+	// called by the getArgs() forced by the FOLNode interface
 	public List<Term> getTerms() {
 		return Collections.unmodifiableList(terms);
 	}
 
 	//
 	// START-Term
+	// 
 	public String getSymbolicName() {
 		return getFunctionName();
 	}
@@ -36,42 +47,53 @@ public class Function implements Term {
 	public boolean isCompound() {
 		return true;
 	}
-
+	
+	// a forced function from the FOLNode interface to get the terms of a function 
 	public List<Term> getArgs() {
 		return getTerms();
 	}
 
+	// a forced function from the FOLNode interface to define the the behavior
 	public Object accept(FOLVisitor v, Object arg) {
 		return v.visitFunction(this, arg);
 	}
 
+	// copy a function
 	public Function copy() {
+		// copy its terms one by one
 		List<Term> copyTerms = new ArrayList<Term>();
 		for (Term t : terms) {
 			copyTerms.add(t.copy());
 		}
+		// return a new instance having the same values of the functions
 		return new Function(functionName, copyTerms);
 	}
 
 	// END-Term
 	//
 
+	// a method to compare two functions
 	@Override
 	public boolean equals(Object o) {
-
+		// same instance
 		if (this == o) {
 			return true;
 		}
+		// different classes
 		if (!(o instanceof Function)) {
 			return false;
 		}
 
+		// two different functions
 		Function f = (Function) o;
-
+		// compare the two function based on the function name and terms
 		return f.getFunctionName().equals(getFunctionName())
 				&& f.getTerms().equals(getTerms());
 	}
 
+	// a hash code function to return a unique value
+	// the function take into consideration the function name  hashcode
+	// and the hashcode of all its terms.
 	@Override
 	public int hashCode() {
 		if (0 == hashCode) {
@@ -84,6 +106,9 @@ public class Function implements Term {
 		return hashCode;
 	}
 
+	// a to String method used to show a function
+	// on this form EX: f(x,y,z)
+	// by aoppending the function name f + ( + the terms +) 
 	@Override
 	public String toString() {
 		if (null == stringRep) {
